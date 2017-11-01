f9af6b32a37299268353b4e189d7a905b5068bd9
James Johnson II <ubani@lawn-143-215-113-12.lawn.gatech.edu>
2017-10-31 21:45:37 -0400

    firebase

--- build/generated/source/r/debug/com/microsoft/band/sdk/sampleapp/accelerometer/R.java
+++ build/generated/source/r/debug/com/microsoft/band/sdk/sampleapp/accelerometer/R.java
@@ -11,31 +11,42 @@ public final class R {
     public static final class attr {
     }
     public static final class dimen {
-        public static final int activity_horizontal_margin=0x7f050000;
-        public static final int activity_vertical_margin=0x7f050001;
+        public static final int activity_horizontal_margin=0x7f060000;
+        public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080005;
-        public static final int btnStart=0x7f080000;
-        public static final int graph1=0x7f080003;
-        public static final int graph2=0x7f080004;
-        public static final int gyroscopeStats=0x7f080002;
-        public static final int txtStatus=0x7f080001;
+        public static final int action_settings=0x7f090005;
+        public static final int btnStart=0x7f090000;
+        public static final int graph1=0x7f090003;
+        public static final int graph2=0x7f090004;
+        public static final int gyroscopeStats=0x7f090002;
+        public static final int txtStatus=0x7f090001;
+    }
+    public static final class integer {
+        public static final int google_play_services_version=0x7f070000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f060000;
-        public static final int app_name=0x7f060001;
-        public static final int start=0x7f060002;
-        public static final int steps=0x7f060003;
+        public static final int action_settings=0x7f040001;
+        public static final int app_name=0x7f040002;
+        public static final int common_google_play_services_unknown_issue=0x7f040000;
+        public static final int default_web_client_id=0x7f040003;
+        public static final int firebase_database_url=0x7f040004;
+        public static final int gcm_defaultSenderId=0x7f040005;
+        public static final int google_api_key=0x7f040006;
+        public static final int google_app_id=0x7f040007;
+        public static final int google_crash_reporting_api_key=0x7f040008;
+        public static final int google_storage_bucket=0x7f040009;
+        public static final int start=0x7f04000a;
+        public static final int steps=0x7f04000b;
     }
     public static final class style {
         /**  API 11 theme customizations can go here. 
@@ -46,9 +57,9 @@ public final class R {
             backward-compatibility can go here.
         
          */
-        public static final int AppBaseTheme=0x7f040000;
+        public static final int AppBaseTheme=0x7f050000;
         /**  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f040001;
+        public static final int AppTheme=0x7f050001;
     }
 }
